002fa4f6a107f03af16117a095d1e4b673a45437
Miguel Moreno <miguel.moreno@fwcsstudents.org>
2024-08-30 12:54:08 -0400

    Add files via upload

---
+++ Moreno_TIY_2_6_Practicing.py
@@ -0,0 +1,6 @@
+message = "Welcome Reader"
+print(f"\t{message}")
+
+famous_person = "Playboi Carti"
+message = f'Quote From {famous_person}:\n"Success is my addiction, I cant get enough."'
+print(message)
