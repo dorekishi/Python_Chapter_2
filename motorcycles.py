96d1c940a67222fccb1e365f9bef303969e6646a
Miguel Moreno <miguel.moreno@fwcsstudents.org>
2024-09-10 11:31:31 -0400

    Add files via upload

---
+++ motorcycles.py
@@ -0,0 +1,83 @@
+motorcycles=['honda',
+             'yamaha',
+             'suzuki']
+print(motorcycles)
+#
+motorcycles[0]='ducati'
+print(motorcycles)
+#
+motorcycles.append('ducati')
+print(motorcycles)
+#
+motorcycles=[]
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+#
+motorcycles=['honda',
+             'yamaha',
+             'suzuki']
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+#
+motorcycles=['honda',
+             'yamaha',
+             'suzuki']
+print(motorcycles)
+del motorcycles[0]
+print(motorcycles)
+#
+motorcycles=['honda',
+             'yamaha',
+             'suzuki']
+print(motorcycles)
+del motorcycles[1]
+print(motorcycles)
+#
+motorcycles=['honda',
+             'yamaha',
+             'suzuki']
+print(motorcycles)
+popped_motorcycles=motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycles)
+#
+motorcycles=['honda',
+             'yamaha',
+             'suzuki']
+last_owned=motorcycles.pop()
+print(f'The last motorcycle I owned was a {last_owned.title()}.')
+#
+motorcycles=['honda',
+             'yamaha',
+             'suzuki']
+first_owned=motorcycles.pop(0)
+print(f'The first motorcycle I owned was a {first_owned.title()}.')
+#
+motorcycles=['honda',
+             'yamaha',
+             'suzuki',
+             'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')
+print(motorcycles)
+#
+motorcycles=['honda',
+             'yamaha',
+             'suzuki',
+             'ducati']
+print(motorcycles)
+too_expensive='ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f'\nA {too_expensive.title()} is too expensive for me.')
+#
+motorcycles=['honda',
+             'yamaha',
+             'suzuki']
+#IT'S SUPPOSED TO HAVE AN INDEX ERROR
+print(motorcycles[3])
+print(motorcycles[-1])
+motorcycles=[]
+print(motorcycles[-1])
